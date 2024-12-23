22a99b7b14d6322010754f9b23c56fb6e9ead9b6
Kim min-jae <mjkim11084@gmail.com>
2024-12-23 21:52:12 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    scanf("%d", &score);
+    if(100>=score && score>=90) {
+        printf("A");
+    } else if(90>score && score>=80) {
+        printf("B");
+    } else if(80>score && score>=70) {
+        printf("C");
+    } else if(70>score && score>=60) {
+        printf("D");
+    } else {
+        printf("F");
+    }
+}
