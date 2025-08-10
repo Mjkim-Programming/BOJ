7d6aca50b0ba11e5e200b76be40e89bb1c1397be
Kim min-jae <mjkim11084@gmail.com>
2025-08-10 15:40:05 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 111276 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for _ in range(T):
+    ps = input()
+    stack = []
+    is_vps = True
+
+    for char in ps:
+        if char == '(':
+            stack.append(char)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                is_vps = False
+                break
+
+    if stack:
+        is_vps = False
+
+    if is_vps:
+        print("YES")
+    else:
+        print("NO")
