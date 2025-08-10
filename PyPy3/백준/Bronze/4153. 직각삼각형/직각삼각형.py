500be80b72736ddc70c30a1218992b1ab5a4afe2
Kim min-jae <mjkim11084@gmail.com>
2025-08-10 11:41:14 +0900

    [Bronze III] Title: 직각삼각형, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,11 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+
+    sides = [a, b, c]
+    sides.sort()
+    if sides[2]**2 == sides[0]**2 + sides[1]**2:
+        print("right")
+    else:
+        print("wrong")
