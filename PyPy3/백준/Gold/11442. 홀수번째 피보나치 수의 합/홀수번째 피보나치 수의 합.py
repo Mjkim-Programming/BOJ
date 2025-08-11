3b5e736eda3a36e39baec0e88108d6c55a44187c
Kim min-jae <mjkim11084@gmail.com>
2025-08-11 23:20:42 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.py
@@ -0,0 +1,27 @@
+MOD = 10**9 + 7
+
+def matmul(A, B):
+    return [[(A[0][0]*B[0][0] + A[0][1]*B[1][0]) % MOD, (A[0][0]*B[0][1] + A[0][1]*B[1][1]) % MOD],
+            [(A[1][0]*B[0][0] + A[1][1]*B[1][0]) % MOD, (A[1][0]*B[0][1] + A[1][1]*B[1][1]) % MOD]]
+
+def matpow(M, n):
+    result = [[1, 0], [0, 1]]
+    base = M
+    while n > 0:
+        if n & 1:
+            result = matmul(result, base)
+        base = matmul(base, base)
+        n >>= 1
+    return result
+
+def fib(n):
+    if n == 0:
+        return 0
+    M = [[1,1],[1,0]]
+    Mn = matpow(M, n-1)
+    return Mn[0][0]
+
+n = int(input())
+k = (n - 1) // 2
+answer = fib(2*k + 2) % MOD
+print(answer)
