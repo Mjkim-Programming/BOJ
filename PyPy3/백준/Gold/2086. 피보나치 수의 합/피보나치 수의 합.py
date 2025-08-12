90c3b15bc7838eceecef8a5055f2a662270bca6b
Kim min-jae <mjkim11084@gmail.com>
2025-08-12 10:29:53 +0900

    [Gold I] Title: 피보나치 수의 합, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/2086. 피보나치 수의 합/피보나치 수의 합.py
@@ -0,0 +1,31 @@
+MOD = 10**9
+
+def matmul(A, B):
+    return [
+        [(A[0][0]*B[0][0] + A[0][1]*B[1][0]) % MOD,
+         (A[0][0]*B[0][1] + A[0][1]*B[1][1]) % MOD],
+        [(A[1][0]*B[0][0] + A[1][1]*B[1][0]) % MOD,
+         (A[1][0]*B[0][1] + A[1][1]*B[1][1]) % MOD]
+    ]
+
+def matpow(M, n):
+    result = [[1, 0], [0, 1]]
+    base = M
+    while n > 0:
+        if n & 1:
+            result = matmul(result, base)
+        base = matmul(base, base)
+        n >>= 1
+    return result
+
+def fib(n):
+    if n == 0:
+        return 0
+    M = [[1, 1], [1, 0]]
+    Mn = matpow(M, n-1)
+    return Mn[0][0]
+
+a, b = map(int, input().split())
+
+res = (fib(b+2) - fib(a+1)) % MOD
+print(res)
