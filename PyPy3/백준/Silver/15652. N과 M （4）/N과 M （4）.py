440998057873eede3d82274d6c7d930b48aaeca0
Kim min-jae <mjkim11084@gmail.com>
2025-08-24 10:32:38 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 112448 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+result = []
+
+def backtrack(start, depth):
+    if depth == M:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(start, N + 1):
+        result.append(i)
+        backtrack(i, depth + 1)
+        result.pop()
+
+backtrack(1, 0)
